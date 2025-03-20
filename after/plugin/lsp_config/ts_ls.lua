24bd31fb4a432dfed1ccf616eeda08394dd0bf09
JoseCisCh <andres.cis.chau@gmail.com>
2025-03-20 11:40:06 -0600

    Change in how the custom LSP configs are done. Now done inside mason.lua in the handler property of mson-lspconfig instead of a separate config file

--- after/plugin/lsp_config/ts_ls.lua
+++
@@ -1,8 +0,0 @@
-require('lspconfig').ts_ls.setup({
-    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    root_dir = require('lspconfig').util.root_pattern("package.json", "tsconfig.json", ".git"),
-    settings = {
-        javascript = { format = { semicolons = "insert" } },
-        typescript = { format = { semicolons = "insert" } },
-    }
-})
--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -7,7 +7,7 @@ local externalConfigurationServers = {
 local debugMode = false
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "eslint", "pyright", "lua_ls", "ts_ls", "html", "cssls"},
+    ensure_installed = { "eslint", "pyright", "lua_ls", "ts_ls", "html", "cssls", "jsonls"},
     handlers = {
         function(server_name)
             if externalConfigurationServers[server_name] then
@@ -16,6 +16,17 @@ require("mason-lspconfig").setup({
                 end
             else
                 require('lspconfig')[server_name].setup({})
+            end
+
+            ts_ls = function()
+                    require('lspconfig').ts_ls.setup({
+                        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+                        root_dir = require('lspconfig').util.root_pattern("package.json", "tsconfig.json", ".git"),
+                        settings = {
+                            javascript = { format = { semicolons = "insert" } },
+                            typescript = { format = { semicolons = "insert" } },
+                        },
+                    })
             end
         end
     }
