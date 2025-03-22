3eb7418036425a67671f986c92203ec31e419ac3
JoseCisCh <andres.cis.chau@gmail.com>
2025-03-21 19:46:28 -0600

    Addition of plenary plugin which was missing for telescope.

--- lua/jcisneros/packer.lua
+++ lua/jcisneros/packer.lua
@@ -6,7 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
+    use 'nvim-lua/plenary.nvim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                            , branch = '0.1.x',
