5fb65718a6d9b8a5b853407fd22a8a78237aa317
JoseCisCh <andres.cis.chau@gmail.com>
2025-03-31 20:26:47 -0600

    Added autocmd to mattn/nvim-emmet in order to enable the pluigin in the following file types:
        - html
        - css
        - xml

--- lua/jcisneros/nvim-emmet.lua
+++ lua/jcisneros/nvim-emmet.lua
@@ -1,3 +1,9 @@
-
-vim.g.user_emmet_install_global = 0;
+-- Disable the global variable bellow to work also in XML.
+-- have to check how to configure the file types to be enabled
+-- automatically.
+--vim.g.user_emmet_install_global = 0;
 vim.g.user_emmet_leader_key ='<C-w>';
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = {"*.html", "*.css", "*.xml"},
+    command = "EmmetInstall"
+})
