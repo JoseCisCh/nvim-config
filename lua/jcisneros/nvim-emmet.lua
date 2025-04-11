30d5a4c73426ce24f0b15253e684ff521b83d781
JoseCisCh <andres.cis.chau@gmail.com>
2025-04-10 20:05:38 -0600

    Change of emmet leader key from <C-w> to <C-q>.

--- lua/jcisneros/nvim-emmet.lua
+++ lua/jcisneros/nvim-emmet.lua
@@ -2,7 +2,7 @@
 -- have to check how to configure the file types to be enabled
 -- automatically.
 --vim.g.user_emmet_install_global = 0;
-vim.g.user_emmet_leader_key ='<C-w>';
+vim.g.user_emmet_leader_key ='<C-q>';
 vim.api.nvim_create_autocmd('FileType', {
     pattern = {"*.html", "*.css", "*.xml"},
     command = "EmmetInstall"
