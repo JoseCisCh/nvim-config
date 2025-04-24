46078984b3dbc3cde2a302fed96b6b1ed215eb81
JoseCisCh <andres.cis.chau@gmail.com>
2025-04-24 10:28:13 -0600

    Commented all the remap shortcodes for code snippets.

--- lua/jcisneros/remap.lua
+++ lua/jcisneros/remap.lua
@@ -71,29 +71,29 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- Allows to change file mod to execution
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
+--vim.keymap.set(
+--    "n",
+--    "<leader>ee",
+--    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+--)
 
-vim.keymap.set(
-    "n",
-    "<leader>ea",
-    "oassert.NoError(err, \"\")<Esc>F\";a"
-)
+--vim.keymap.set(
+--    "n",
+--    "<leader>ea",
+--    "oassert.NoError(err, \"\")<Esc>F\";a"
+--)
 
-vim.keymap.set(
-    "n",
-    "<leader>ef",
-    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
-)
+--vim.keymap.set(
+--    "n",
+--    "<leader>ef",
+--    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
+--)
 
-vim.keymap.set(
-    "n",
-    "<leader>el",
-    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
-)
+--vim.keymap.set(
+--    "n",
+--    "<leader>el",
+--    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
+--)
 
 vim.keymap.set("n", "<leader>ca", function()
     require("cellular-automaton").start_animation("make_it_rain")
