57ae0cb3eb878ee2ae309879d8c61755ba5143ce
JoseCisCh <andres.cis.chau@gmail.com>
2025-07-02 21:42:56 -0600

    config(Telescope): Added custm command file search
        - Added this in telescope.lua, command to use fd with some
          specificaitions like including hidden files but excluding .git.

--- after/plugin/telescope/telescope.lua
+++ after/plugin/telescope/telescope.lua
@@ -1,5 +1,15 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n',
+    '<leader>pf',
+    function ()
+        builtin.find_files({
+            find_command = { "fd", "--type", "f", "--hidden", "--exclude", ".git", "--strip-cwd-prefix" }
+        })
+    end,
+    {
+        desc = 'Telescope find files'
+    }
+)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
