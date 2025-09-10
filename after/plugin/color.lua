63b9e0d07368c88a8ee4d5ac20a9ea52bb607236
JoseCisCh <andres.cis.chau@gmail.com>
2025-09-09 21:25:00 -0600

    Theme: Color scheme changes.

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,8 +1,8 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine"
+    color = color or "onedark"
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#37353E" })
     -- Next line is necessary for onwdark colorscheme
     --vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 end
--- after/plugin/telescope/telescope.lua
+++ after/plugin/telescope/telescope.lua
@@ -14,3 +14,30 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- Configuring particlular Telescope colors.
+-- Function to make Telescope transparent
+local function telescope_transparent()
+  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeMatching", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "none" })
+end
+
+-- Reapply after colorscheme
+--vim.api.nvim_create_autocmd("ColorScheme", {
+--  callback = telescope_transparent,
+--})
+
+-- Call once on startup
+telescope_transparent()
